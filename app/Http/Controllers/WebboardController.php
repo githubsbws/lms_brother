269367381800893acf0f4b6c7dbd88908603d561
dis <stpdis1503@gmail.com>
2024-01-31 17:31:10 +0700

    webboard

--- app/Http/Controllers/WebboardController.php
+++ app/Http/Controllers/WebboardController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BBii_forum;
 
 class WebboardController extends Controller
 {
     function webboard(){
-        return view("webboard.webboard");
+        $web = BBii_forum::get();
+        return view("webboard.webboard",['web'=>$web]);
     }
 }
