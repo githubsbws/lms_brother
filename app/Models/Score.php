d4ebf43a0e116adb288530187f695e3394777dca
WannisaBun02 <sawb35114@gmail.com>
2024-02-05 16:33:10 +0700

    score_model

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -10,8 +10,23 @@ class Score extends Model
     use HasFactory;
 
     protected $table = 'score';
-
-    protected $primarykey = 'score_id';
+    protected $primaryKey = 'score_id';
+    protected $fillable = [
+        'lesson_id',
+        'user_id',
+        'course_id',
+        'type',
+        'score_number',
+        'score_total',
+        'score_past',
+        'create_by',
+        'create_date',
+        'update_by',
+        'update_date',
+        'active',
+    ];
+    const CREATED_AT = 'create_date'; // Custom created_at column
+    const UPDATED_AT = 'update_date'; // Custom update_at column
 
     public static function findById($id)
     {
