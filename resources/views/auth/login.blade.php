04f7a4db11d4ffe66eda66dc8d3c2ffbe5845d28
githubsbws <githubsbws@gmail.com>
2024-06-24 16:27:59 +0700

    captcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,7 @@
                                     </div>
                                     <div class="form-group">
                                         <div class="col-sm-6 col-sm-offset-3" style="padding: 0;">
-                                            <div class="g-recaptcha" data-sitekey="6Le2CgAqAAAAAE3EFyWSAQz4U0hxOT-wpiJw4jky"></div>
+                                            <div class="g-recaptcha" data-sitekey="6Le3zv8pAAAAACNf85umY6OIKbJYQOJMzoLjR8ZK" data-action="LOGIN"></div>
                                         </div>
                                     </div>
                                    
@@ -96,6 +96,6 @@
             </div>
         </div>
     </div>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
 </body>
 @endsection
