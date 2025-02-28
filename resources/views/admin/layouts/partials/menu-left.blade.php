2aa8a53800a6e9fc02f40897595ea6702f24ab83
githubsbws <githubsbws@gmail.com>
2025-02-28 15:19:48 +0700

    menu

--- resources/views/admin/layouts/partials/menu-left.blade.php
+++ resources/views/admin/layouts/partials/menu-left.blade.php
@@ -416,7 +416,7 @@
                         </li>
                         <li class="nav-item">
                             <a href="{{url('/asc')}}" class="nav-link {{ request()->is('asc') ? 'active' : '' }}">
-                                <p>เพิ่มASC</p>
+                                <p>เพิ่มCompany</p>
                             </a>
                         </li>
                         <li class="nav-item">
